7abc8b9c6e65a5d41efeacdc92fa25fd9fc7a411
shortnick <@BetterNameTaken>
2017-12-26 15:32:07 -0800

    Single low point selected !good!
    Things to know: psycopg2.cursor.fetchall() should only be called once
    and assigned to a variable.
    
    Current script will parse all points selected within a voxel and assign
    the lowest one to an output list.

--- Zbintest.py
+++ Zbintest.py
@@ -11,10 +11,16 @@
 #from __future__ import division
 import psycopg2
 import logging
+import inspect
 
 
 binCount = 4
 binMax = abs(binCount)
+
+####DEBUG CODE
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %s" % (attr, getattr(obj, attr)))
 
 
 #Range & Step variables
@@ -41,6 +47,7 @@ zbinHi = -9
 nbinList = []
 ebinList = [] 
 zbinList = []
+outputBin = []
 
 # Functions
 
@@ -104,8 +111,18 @@ def makeTupleList(binSet, minAA, stepAA, maxAA):
 
 def voxelSelector(nbinLow, nbinHi, ebinLow, ebinHi, zbinLow, zbinHi):
 	maniP.execute("""SELECT e, n, z, rgby FROM ptsTiny WHERE (n BETWEEN %(nmin)s and %(nmax)s) AND (e BETWEEN %(emin)s AND %(emax)s) AND (z BETWEEN %(zmin)s AND %(zmax)s);""", {'nmin':nbinLow, 'nmax':nbinHi, 'emin':ebinLow, 'emax':ebinHi, 'zmin': zbinLow, 'zmax': zbinHi})
-	if len(maniP.fetchall()) > 0:
-		logging.debug("Points selected--"+str(maniP.fetchall()))
+	collectedResults = maniP.fetchall()
+	if len(collectedResults) > 0:
+		logging.debug("Voxel with points\n" + str(collectedResults))
+		threshold = (-99999, -99999, 1000000000000, -99999)
+		for q in collectedResults:
+			alpha, beta, charlie, delta = threshold
+			a, b, c, d = q
+			if c < charlie:
+				threshold = q
+		logging.info("Low point " +str(threshold))	
+		outputBin.append(threshold)		
+#	print(maniP.fetchall())
 
 
 
@@ -119,37 +136,57 @@ def voxelSelector(nbinLow, nbinHi, ebinLow, ebinHi, zbinLow, zbinHi):
 logging.basicConfig(filename='VoxelizerDebugs.log',level=logging.DEBUG)
 
 
-try:
-	conn = psycopg2.connect("dbname='quest' user='basic' password='postgrescorvus'")
-	logging.info("Database Connected")
+conn = psycopg2.connect("dbname='quest' user='basic' password='postgrescorvus'")
+logging.info("Database Connected")
 
-	with conn.cursor() as maniP:
+with conn.cursor() as maniP:
 
-		maniP.execute(
-			"""(select (max(n)-min(n)),
-			(max(e)-min(e)),
-			(max(z)-min(z))
-			from quest.public.ptsTiny)""")
-		gapN, gapE, gapZ = (maniP.fetchall()[0])
-		#why can't (maniP.fetchall()[0]) be printed? it's 'out of range'
-		logging.debug("gapN = %s, gapE = %s, gapZ =%s"%(gapN, gapE, gapZ))
-		maniP.execute(
-			"""select max(n), min(n), count(n), max(e), min(e), count(e), max(z), min(z), count(z) FROM quest.public.ptsTiny"""
-			)
-		maxN, minN, countN, maxE, minE, countE, maxZ, minZ, countZ = (maniP.fetchall()[0])
-		logging.debug(" \n maxN = %s, minN = %s, countN = %s,  \n maxE = %s, minE = %s, countE = %s,  \n maxZ = %s, minZ = %s, countZ = %s"%(maxN, minN, countN, maxE, minE, countE, maxZ, minZ, countZ))
+	maniP.execute(
+		"""(select (max(n)-min(n)),
+		(max(e)-min(e)),
+		(max(z)-min(z))
+		from quest.public.ptsTest)""")
+	gapN, gapE, gapZ = (maniP.fetchall()[0])
+	#why can't (maniP.fetchall()[0]) be printed? it's 'out of range'
+	logging.debug("gapN = %s, gapE = %s, gapZ =%s"%(gapN, gapE, gapZ))
+	maniP.execute(
+		"""select max(n), min(n), count(n), max(e), min(e), count(e), max(z), min(z), count(z) FROM quest.public.ptsTest"""
+		)
+	maxN, minN, countN, maxE, minE, countE, maxZ, minZ, countZ = (maniP.fetchall()[0])
+	logging.debug(" \n maxN = %s, minN = %s, countN = %s,  \n maxE = %s, minE = %s, countE = %s,  \n maxZ = %s, minZ = %s, countZ = %s"%(maxN, minN, countN, maxE, minE, countE, maxZ, minZ, countZ))
 
-	
+
 	if countN != countZ or countZ != countE:
 		logging.warning("Record counts return mismatched numbers.")
 
 
 	initialize()
-	
 
+	makeTupleList("n", minN, stepN, maxN)
+	makeTupleList("e", minE, stepE, maxE)
 	makeTupleList("z", minZ, stepZ, maxZ)
-	
+	if (len(zbinList) != len(ebinList)) or (len(ebinList) != len(nbinList)) or (len(zbinList) != len(nbinList)):
+		logging.warning("BinLists are different lengths")
 
-except:
-	logging.warning("Unable to connect to the database")
 
+	logging.info("Beginning parsing and selection")
+	for n in nbinList:
+			#interpret tuple for nbin
+			nbinLow = n[0]
+			nbinHi = n[1]
+
+			for e in ebinList:
+				ebinLow = e[0]
+				ebinHi = e[1]
+			#interpret tuple for ebin
+				for z in zbinList:
+					zbinLow = z[0]
+					zbinHi = z[1]
+					#outter select for lowest point
+					#inner select for the three tuples
+					#append outter select to new table
+					voxelSelector(nbinLow, nbinHi, ebinLow, ebinHi, zbinLow, zbinHi)
+	logging.info("\n \n Final Output Selection")
+	logging.info(outputBin)
+									
+	logging.info("Finished completely")
